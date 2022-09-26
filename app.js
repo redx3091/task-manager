1f30c181ae5ce1328f5038b04bd01cb00e78d433
Christian A <christiana@iMac-de-Christian.local>
2022-09-26 15:15:03 -0500

    ADD Connection DB and enviroment varibales

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const tasks = require('./routers/task');
+const connectDB = require('./db/connect');
+require('dotenv').config();
 
 //*middleware
 app.use(express.json());
@@ -14,4 +16,13 @@ app.use('/api/v1/tasks', tasks);
 
 const port = 3000;
 
-app.listen(port, console.log(`server is listening on port ${port}...`));
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI);
+    app.listen(port, console.log(`server is listening on port ${port}...`));
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
